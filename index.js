d661081d2980212f206edce47f597b91ba96c9bd
Asma505 <sonali.hcn@gmail.com>
2023-06-11 16:42:23 +0600

    code added to set the user role as instructor

--- index.js
+++ index.js
@@ -67,6 +67,18 @@ async function run() {
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
     })
+    
+    app.patch('/users/instructor/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          role: "Instructor"
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
 
     // details data api
     app.get('/details', async(req, res)=>{

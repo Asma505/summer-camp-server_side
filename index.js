c72e0988149e5ded9727c512b63fde0dcabd9581
Asma505 <sonali.hcn@gmail.com>
2023-06-13 00:11:02 +0600

    code added to add payment method

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY) 
 const port = process.env.PORT || 5000;
 
 
@@ -205,6 +206,19 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = await courseCollection.deleteOne(query);
       res.send(result);
+    })
+
+    // payment intent
+    app.post('/create-payment-intent', verifyJWT, async(req, res)=>{
+      const {price} = req.body;
+      const amount = parseInt(price*100);
+      const paymentIntent = await stripe.paymentIntents.create({amount: amount,
+      currency: 'usd',
+      payment_method_types: ['card']
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
     })
 
     // Send a ping to confirm a successful connection
